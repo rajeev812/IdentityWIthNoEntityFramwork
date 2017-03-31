c30e7aa84238328904cc1b9ecf74dd0dbb6a92a6
rajeev812 <raj.812@hotmail.com>
2017-03-31 14:54:25 +0530

    AS

---
+++ IdentityWithoutEntityFramwork/Controllers/ConnectionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace IdentityWithoutEntityFramwork.Controllers
+{
+    public class ConnectionController : Controller
+    {
+        // GET: Connection
+        public Guid UserId
+        {
+            get
+            {
+                if (User.Identity.IsAuthenticated)
+                    return Guid.Parse(User.Identity.GetUserId());
+
+                return Guid.Empty;
+            }
+        }
+    }
+}
---
+++ IdentityWithoutEntityFramwork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityWithoutEntityFramwork.Startup))]
+namespace IdentityWithoutEntityFramwork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
